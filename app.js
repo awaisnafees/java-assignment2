90cda1720a08258e848a3ff8f838b1ef0d21458a
awaisnafees <awaisnafees96@gmail.com>
2020-06-21 06:22:29 -0700

    assignment 2 ready

---
+++ app.js
@@ -0,0 +1,91 @@
+// var x= "Enter your name";
+// var y="your name is:"
+// var a=+prompt(x,y);
+// function greeting(awais){
+//         alert("fullname")
+//     }
+//     greeting("");
+
+
+// var Mobile=prompt("Enter your Mobile name")
+// var arr = ["sam sung"]
+// for (var i=0;i<arr.length;i++){
+//     if (arr[i] === Mobile){
+//         alert("my favourite phone is galaxy")
+//     }
+// }
+
+
+
+// var a="Pakistani";
+// console.log(a.charAt(7));
+
+// var a="Hello World";
+// console.log(a.charAt(9));
+
+// var a="Pakistani";
+// console.log(a.charAt(3));
+
+
+// var x= "Enter your name";
+// var y="your name is:"
+// var a=+prompt(x,y);
+// function greeting(awais){
+//         alert("0")
+//     }
+//     greeting("");
+
+
+
+
+// var para = "Hyderabad";
+// var para1 = para.replace("Hyderabad","Islamqabad");
+// console.log(para1)
+
+
+
+// var a = ("ali and sami are best friends. they play cricket and football together");
+// document.write(a)
+// a.toString()
+
+
+// var foot=("472");
+// document.write(foot)
+
+// var name = prompt();
+// name = name.toUpperCase();
+// document.write(name)
+
+
+// var user input = +prompt("javascript")
+// var Titlecase = ("Javascript");
+// document.write(user input)
+
+
+// var num = "35.36";
+// var inpt = num.replace("35.36","3536");  
+// console.log(inpt)
+
+
+// var name= +prompt("Enter your Name");
+// if (name === "@","!","."){
+//     alert("Please enter a valid Name")
+// }
+
+
+// var Bakery=prompt("")
+// var arr = ["cake","cookie", "pastry"]
+// for (var i=0;i<arr.length;i++){
+//     if (arr[i] === Bakery){
+//         alert("cake is available")
+//     }
+
+//     else{
+//         alert("We are Soory.pastry is not available in our baqkery")
+//     }
+    
+// }
+
+
+
+
