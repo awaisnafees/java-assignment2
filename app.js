6b2a7c7d833c910e1716eecf65a4b365e4835296
awaisnafees <awaisnafees96@gmail.com>
2020-06-22 11:16:17 -0700

    jav2 assignment complet

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+
+// task 21 to 25
+
+
+
 // var x= "Enter your name";
 // var y="your name is:"
 // var a=+prompt(x,y);
@@ -38,9 +43,9 @@
 
 
 
-// var para = "Hyderabad";
-// var para1 = para.replace("Hyderabad","Islamqabad");
-// console.log(para1)
+// var city = "Hyderabad";
+// var city1 = city.replace("Hyderabad","Islamabad");
+// console.log(city1)
 
 
 
@@ -74,18 +79,83 @@
 
 
 // var Bakery=prompt("")
-// var arr = ["cake","cookie", "pastry"]
+// var arr = ["cake","pastry"]
 // for (var i=0;i<arr.length;i++){
 //     if (arr[i] === Bakery){
 //         alert("cake is available")
 //     }
+    
 
 //     else{
-//         alert("We are Soory.pastry is not available in our baqkery")
+//         alert("We are Soory. pastry is not available in our baqkery")
 //     }
     
 // }
 
+// var name= +prompt("Enter your password");
+// if (name === "1","2","3"){
+//     alert("password can not begin with num Please enter a valid password")
+// }
+
+// var word = ("university of karachi");
+// var check;
+
+// for ( var i= word.length -1; i >=0; i--){
+// console.log(word[i])
+// }
 
 
 
+// var a="Pakistan";
+// console.log(a.charAt(7));
+
+
+
+
+// tast 26 to 30
+
+
+// var  num = 3.45214;
+// var round = Math.round(num);
+// var round = Math.floor(num);
+// var round = Math.ceil(num);   
+// console.log(round)
+
+// var num = -2.673
+// var round = Math.round(num);
+// var round = Math.floor(num);
+// var round = Math.ceil(num);
+// console.log(round)
+
+
+// var a="-4 is 4";
+// document.write("the absolute value of"+a)
+
+// var value="4";
+// var value1="6";
+// alert("random dice value:"+value)
+// document.write("random dice value:"+value1)
+
+
+// var headuser =("2");
+// var tailuser =("1");
+// var toss = Math.random() * 2;
+// var floor = Math.floor(toss)
+// if(floor === 0){
+//     alert("" + headuser + " random coin value : Head")
+// }else {
+//     alert(""  + tailuser + " random coin value : Tail ")
+// }
+
+
+// var man=+prompt("Enter your Weight");
+// var bod="kg";
+// var body=man+bod;
+// document.write("the weight of user is"+body)
+
+
+
+// var x=prompt("Enter a number between 1 and 10")
+// for(x;x<10;x++){
+//     document.write(x)
+// }
